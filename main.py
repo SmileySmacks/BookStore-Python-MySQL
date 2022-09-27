16583160d75c2a8d280f5278eda81e1a52224267
Your Name <you@example.com>
2022-09-27 09:57:32 -0500

    Created tables within DB, from Pycharm

--- main.py
+++ main.py
@@ -25,16 +25,38 @@ def create_database(connection, query):
         print(f"Error: '{err}'")
 
 
-
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
 #Commands
 
 create_db = """
 create database Bookstore;"""
 
+create_table_book_info = """
+create table Book_Information(
+Book_ID int primary key,
+Title varchar(30) not null,
+Genre varchar(20) not null,
+Price varchar(10) not null);"""
+
+create_table_employee_info = """
+create table Employee_Information(
+Employee_ID varchar(5) primary key,
+Name varchar(30) not null,
+Position varchar(30) not null,
+Schedule varchar(30) not null,
+Benefits varchar(3) not null);"""
+
 
 
 
 # Callouts
 connection = create_server_connection("localhost", "root", "student", "Bookstore")
-
+execute_query(connection, create_table_employee_info)
